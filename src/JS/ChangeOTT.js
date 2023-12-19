5ba0663e99e441085b1c6c87eb9fba8c56cfdac4
wonhyuna <hyunawon417@gmail.com>
2023-12-19 21:37:22 +0900

    Feat: 마이페이지 구독 중인 OTT 선택 페이지 세팅(#2)

---
+++ src/JS/ChangeOTT.js
@@ -0,0 +1,16 @@
+import '../App.css';
+import '../CSS/ChangeOTT.css'
+import netflix_word from '../img/netflix_word.png';
+import watcha from '../img/watcha.png';
+import wavve from '../img/wavve.png';
+import coupang_play from '../img/coupang_play.png';
+import tving from '../img/tving.png';
+import apple_tv from '../img/apple_tv.png';
+import disney from '.disney.png';
+import laftel from '../img/laftel.png';
+import prime_video from '../img/prime_video.png';
+import series_on from '../img/series_on.png';
+import google_play from '../img/google_play.png';
+import u_plus from '../img/u_plus.png';
+import close_gray from '../img/close_gray.png';
+
