3889ad5f69eaea30e8fb96ffc51f9560ca52aad9
wonhyuna <hyunawon417@gmail.com>
2023-12-29 01:11:16 +0900

    Feat: 마이페이지 OTT 선택 전체 취소하기 버튼 클릭 시, 색상 변경 (#9)

--- src/JS/ChangeOTT.js
+++ src/JS/ChangeOTT.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import '../App.css';
 import '../CSS/ChangeOTT.css'
 import PickOTTColor from './PickOTTColor';
@@ -16,63 +17,69 @@ import u_plus from '../img/u_plus.png';
 import close_gray from '../img/close_gray.png';
 
 const ChangeOTT = () => {
+  const [resetStyles, setResetStyles] = useState(false);
+
+  const handleAllDelete = () => {
+    setResetStyles(!resetStyles);
+  };
+
   return (
     <div className="change_ott">
       <div className="ott_title">
         <h1>구독 중인 <span>OTT</span>를 선택해주세요</h1>
       </div>
       <div className = "choose_ott">
-         <PickOTTColor className = "ott_pick_logo">
-          <img src = {netflix_word} className='logo_img'></img>
-          <span className='logo_name'>넷플릭스</span>
-         </PickOTTColor>
-         <PickOTTColor className = "ott_pick_logo">
-          <img src = {watcha} className='logo_img'></img>
-          <span className='logo_name'>왓챠</span>
-         </PickOTTColor>
-         <PickOTTColor className = "ott_pick_logo">
-          <img src = {disney} className='logo_img'></img>
-          <span className='logo_name'>디즈니+</span>
-         </PickOTTColor>
-         <PickOTTColor className = "ott_pick_logo">
-          <img src = {tving} className='logo_img'></img>
-          <span className='logo_name'>티빙</span>
-         </PickOTTColor>
-         <PickOTTColor className = "ott_pick_logo">
-          <img src = {wavve} className='logo_img'></img>
-          <span className='logo_name'>웨이브</span>
-         </PickOTTColor>
-         <PickOTTColor className = "ott_pick_logo">
-          <img src = {coupang_play} className='logo_img'></img>
-          <span className='logo_name'>쿠팡플레이</span>
-         </PickOTTColor>
-         <PickOTTColor className = "ott_pick_logo">
-          <img src = {laftel} className='logo_img'></img>
-          <span className='logo_name'>라프텔</span>
-         </PickOTTColor>
-         <PickOTTColor className = "ott_pick_logo">
-          <img src = {apple_tv} className='logo_img'></img>
-          <span className='logo_name'>Apple TV</span>
-         </PickOTTColor>
-         <PickOTTColor className = "ott_pick_logo">
-          <img src = {prime_video} className='logo_img'></img>
-          <span className='logo_name'>아마존 프라임 비디오</span>
-         </PickOTTColor>
-         <PickOTTColor className = "ott_pick_logo">
-          <img src = {google_play} className='logo_img'></img>
-          <span className='logo_name'>Google Play 무비</span>
-         </PickOTTColor>
-         <PickOTTColor className = "ott_pick_logo">
-          <img src = {u_plus} className='logo_img'></img>
-          <span className='logo_name'>U+ TV</span>
-         </PickOTTColor>
-         <PickOTTColor className = "ott_pick_logo">
-          <img src = {series_on} className='logo_img'></img>
-          <span className='logo_name'>네이버 시리즈온</span>
-         </PickOTTColor>
+         <PickOTTColor className = 'ott_pick_logo' resetStyles={resetStyles}>
+            <img src = {netflix_word} className='logo_img'></img>
+            <span className='logo_name'>넷플릭스</span>  
+        </PickOTTColor>
+        <PickOTTColor className = 'ott_pick_logo' resetStyles={resetStyles}>
+            <img src = {watcha} className='logo_img'></img>
+            <span className='logo_name'>왓챠</span>  
+        </PickOTTColor>
+        <PickOTTColor className = 'ott_pick_logo' resetStyles={resetStyles}>
+            <img src = {disney} className='logo_img'></img>
+            <span className='logo_name'>디즈니+</span>   
+        </PickOTTColor>
+        <PickOTTColor className = 'ott_pick_logo' resetStyles={resetStyles}>
+            <img src = {tving} className='logo_img'></img>
+            <span className='logo_name'>티빙</span>  
+        </PickOTTColor>
+        <PickOTTColor className = 'ott_pick_logo' resetStyles={resetStyles}>
+            <img src = {wavve} className='logo_img'></img>
+            <span className='logo_name'>웨이브</span>   
+        </PickOTTColor>
+        <PickOTTColor className = 'ott_pick_logo' resetStyles={resetStyles}>
+            <img src = {coupang_play} className='logo_img'></img>
+            <span className='logo_name'>쿠팡플레이</span>
+        </PickOTTColor>
+        <PickOTTColor className = 'ott_pick_logo' resetStyles={resetStyles}>
+            <img src = {laftel} className='logo_img'></img>
+            <span className='logo_name'>라프텔</span>
+        </PickOTTColor>
+        <PickOTTColor className = 'ott_pick_logo' resetStyles={resetStyles}>
+            <img src = {apple_tv} className='logo_img'></img>
+            <span className='logo_name'>Apple TV</span>  
+        </PickOTTColor>
+        <PickOTTColor className = 'ott_pick_logo' resetStyles={resetStyles}>
+            <img src = {prime_video} className='logo_img'></img>
+            <span className='logo_name'>아마존 프라임 비디오</span>  
+        </PickOTTColor>
+        <PickOTTColor className = 'ott_pick_logo' resetStyles={resetStyles}>
+            <img src = {google_play} className='logo_img'></img>
+            <span className='logo_name'>Google Play 무비</span>   
+        </PickOTTColor>
+        <PickOTTColor className = 'ott_pick_logo' resetStyles={resetStyles}>
+            <img src = {u_plus} className='logo_img'></img>
+            <span className='logo_name'>U+ TV</span>  
+        </PickOTTColor>
+        <PickOTTColor className = 'ott_pick_logo' resetStyles={resetStyles}>
+            <img src = {series_on} className='logo_img'></img>
+            <span className='logo_name'>네이버 시리즈온</span>   
+        </PickOTTColor>
       </div>
 
-      <button className='all_delete'>
+      <button className='all_delete' onClick={handleAllDelete}>
         <img src = {close_gray} className='delete_close'></img>
         <span className='delete_word'>전체 취소하기</span>
       </button>
--- src/JS/PickOTTColor.js
+++ src/JS/PickOTTColor.js
@@ -1,34 +1,38 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
-function PickOTTColor({children}){
+function PickOTTColor({ children, resetStyles }) {
   const [divState, setDivState] = useState(false);
   const [filterState, setFilterState] = useState('none');
-  const [backState, setBackState] = useState('none');
-  
-  const handleDivClick = ()=>{
-    const newFilterState = divState ? 'none' : 'grayscale(100%)';
-    const newBackState = divState ? 'var(--neutral2)' : 'var(--orange_400)';
+  const [backState, setBackState] = useState('var(--neutral2)');
 
+  useEffect(() => {
+    // 전체 취소하기 버튼이 눌렸을 때
+    if (resetStyles) {
+      setDivState(true);
+      setFilterState('grayscale(100%)');
+      setBackState('var(--orange_400)');
+    }
+  }, [resetStyles]);
+
+  const handleDivClick = () => {
+    setFilterState(divState ? 'none' : 'grayscale(100%)');
+    setBackState(divState ? 'var(--neutral2)' : 'var(--orange_400)');
     setDivState(!divState);
-    setFilterState(newFilterState);
-    setBackState(newBackState);
-  }
+  };
 
   const divStyle = {
     filter: filterState,
-    backgroundColor: backState
-  }
+    backgroundColor: backState,
+  };
 
   return (
     <div 
-      style = {divStyle} 
-      className='ott_pick_logo'
-      onClick = {handleDivClick}>
-
+      style={divStyle} 
+      className={`ott_pick_logo${divState ? ' change_styles' : ''}`} 
+      onClick={handleDivClick}>
       {children}
     </div>
-  )
-
+  );
 }
 
 export default PickOTTColor;
