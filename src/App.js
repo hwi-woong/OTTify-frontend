c544b43176ec73d619c2680f342918d3e5f54de8
wonhyuna <hyunawon417@gmail.com>
2023-12-30 22:11:36 +0900

    Fix: 전체 페이지 이동 시, 상단으로 고정시키기 (#15)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ScrollToTop from './JS/ScrollToTop';
 import Mypage from './JS/Mypage';
 import ChangeOTT from './JS/ChangeOTT';
 import MyWrite from './JS/MyWrite';
@@ -8,12 +9,14 @@ import MyFavorite from './JS/MyFavorite';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Mypage />} />
-        <Route path="/ChangeOTT" element={<ChangeOTT />} />
-        <Route path="/MyWrite" element={<MyWrite/>}/>
-        <Route path="/MyFavorite" element={<MyFavorite/>}/>
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Mypage />} />
+          <Route path="/ChangeOTT" element={<ChangeOTT />} />
+          <Route path="/MyWrite" element={<MyWrite/>}/>
+          <Route path="/MyFavorite" element={<MyFavorite/>}/>
+        </Routes>
+      </ScrollToTop>
     </BrowserRouter> 
   );
 }
